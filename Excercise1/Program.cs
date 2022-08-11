b15a175fa38552dcc9296f1de9bf352db06b764f
Anna <pgav89@mail.ru>
2022-08-11 11:55:53 +1000

    Algorithm

--- Excercise1/Program.cs
+++ Excercise1/Program.cs
@@ -6,3 +6,31 @@
 // ["hello", "2","world",":)"] → ["2",":)"]
 // ["1234", "1567","-2","computer science"] → ["-2"]
 // ["Russia", "Denmark","Kazan"] → []
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] myArray = new string[size];
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"Введите число массива с индексом [{i}]=");
+    myArray[i] = Convert.ToString(Console.ReadLine());
+}
+int ResultArrayLenght = 0; //Длина результирующего массива
+int IndexFirstElement = 0; //Индекс первого результирующего элемента
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i].Length > 0 && myArray[i].Length <= 3) ResultArrayLenght++;
+}
+Console.WriteLine($"Изначальный массив: [{String.Join(";", myArray)}]");
+string[] finalArray = new string[ResultArrayLenght];
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i].Length > 0 && myArray[i].Length <= 3)
+    {
+        finalArray[IndexFirstElement] = myArray[i];
+        IndexFirstElement++;
+    }
+    if (IndexFirstElement == ResultArrayLenght) break;
+}
+Console.WriteLine($"Отсортированный массив: [{String.Join(",", finalArray)}]");
